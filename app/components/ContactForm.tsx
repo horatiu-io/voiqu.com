b31b2228ca9e7093759e31edf0b0f603200be060
voiqu <voiqu.horatiu@gmail.com>
2025-08-03 23:52:13 +0300

    Update ContactForm.tsx

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -8,6 +8,7 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Label } from "@/components/ui/label"
 
+// THIS is the correct encode function signature!
 function encode( { [key: string]: any }) {
   return Object.keys(data)
     .map(
